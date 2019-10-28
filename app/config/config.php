c04aa025cc86a5ec24c9a6df07358d9688a7b4bb
angellee177 <angelriapurnamasari17@gmail.com>
2019-10-28 20:14:08 +0700

    [angel] framework done

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define("DB_USER", 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'tmvc');
+
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+    define('URLROOT', 'http://localhost/php_MVC/');
+    define('SITENAME', 'oopPHP');
---
+++ app/views/pages/index.php
@@ -0,0 +1,3 @@
+<?php require APPROOT . '/views/layouts/header.php';?>
+    <h1><?php echo $data['title'];?></h1>
+<?php require APPROOT . '/views/layouts/footer.php';?>
