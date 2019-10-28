533619203e30bd35480fa5f9b9520124e40d79f3
Angel Ria Purnamasari <40418196+angellee177@users.noreply.github.com>
2019-10-28 20:44:24 +0700

    Update config.php
    change config

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
     define('DB_HOST', 'localhost');
-    define("DB_USER", 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'tmvc');
+    define("DB_USER", '__YOUR_USER_NAME__');
+    define('DB_PASS', '__YOUR__USER__PASSWORD__');
+    define('DB_NAME', '__YOUR__DB_NAME__');
 
 
     define('APPROOT', dirname(dirname(__FILE__)));
-    define('URLROOT', 'http://localhost/php_MVC/');
+    define('URLROOT', '__YOUR_LOCALHOST_URL__');
     define('SITENAME', 'oopPHP');
